fd55d19e562566696cb2a049cb71406c93cf5ba4
Alessandra Gherardelli <AlexGherardelli@users.noreply.github.com>
2024-05-31 15:22:15 +0200

    Updated gitignore. Testing R sample load

--- sample_python_load.py
+++ sample_python_load.py
@@ -1,8 +1,13 @@
+"""
+Reads Household Data from Data Bridges. The script uses the DataBridgesShapes class from the data_bridges_utils module to interact with the Data Bridges API and retrieve various datasets, including:
+- Household survey data
+- GORP (Global Operational Response Plan) data
+- Exchange rates and prices for Afghanistan
+- IPC and equivalent food security Data
 
+The script demonstrates how to use the DataBridgesShapes class to fetch these datasets and print the first few rows of the resulting pandas DataFrames.
 """
-Read Household Data from Data Bridges and load it into STATA.
-Only works if user has STATA 18+ installed and added to PATH.
-"""
+
 from data_bridges_utils import DataBridgesShapes
 
 CONFIG_PATH = r"data_bridges_api_config.yaml"
