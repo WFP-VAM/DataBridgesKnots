518c652d267a84039de44b014e5b695de67ca316
Alessandra Gherardelli <AlexGherardelli@users.noreply.github.com>
2024-05-31 16:19:02 +0200

    Minor fixes to setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'),
     long_description = f.read()
 
 setup(
-    name='DataBridgesUtils',
+    name='data_bridges_utils',
     version='1.0.0',
     description='Wrapper for Data Bridges API client',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/your_org/DataBridgesUtils',
+    url='https://github.com/WFP-VAM/DataBridgesUtils',
     author='Alessandra Gherardelli, Valerio Giuffrida',
     author_email='alessandra.gherardelli@wfp.org, valerio.giuffrida@wfp.org',
     license='GNU General Public License v3 or later (GPLv3+)',
@@ -26,7 +26,7 @@ setup(
         'pandas>=2',
         'pystata',
         'stata-setup',
-        'DataBridgesAPI @ git+ssh://git@github.com:WFP-VAM/DataBridgesAPI.git',
+        'DataBridgesAPI @ git+ssh://git@github.com:WFP-VAM/DataBridgesAPI.git@dev',
     ],
     extras_require={
         'dev': [
@@ -36,13 +36,14 @@ setup(
             'pip-tools',
             'pytest',
         ],
-        'STATA': [
+        'data-bridges-utils': [],
+        'data-bridges-utils-STATA': [
             'stata-setup',
             'pystata',
         ],
-        'R': [],
+        'data-bridges-utils-R': [],
     },
     project_urls={
-        'Source': 'https://github.com/your_org/DataBridgesUtils',
+        'Source': 'https://github.com/WFP-VAM/DataBridgesUtils',
     },
 )
