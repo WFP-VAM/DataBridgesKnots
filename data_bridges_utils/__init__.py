e65fa4926dee9aa79a3e4b2263fd7baa324a0f93
Alessandra Gherardelli <AlexGherardelli@users.noreply.github.com>
2024-06-10 15:45:48 +0200

    Labelling functions - some working better than others!

--- data_bridges_utils/__init__.py
+++ data_bridges_utils/__init__.py
@@ -5,6 +5,6 @@ Wrapper for DataBridges client.
 """
 
 from .get_data import DataBridgesShapes
-from .labels import map_value_labels
+from .labels import get_column_labels, get_value_labels, map_value_labels
 
 __all__ = ['DataBridgesShapes', 'labels']
--- data_bridges_utils/labels.py
+++ data_bridges_utils/labels.py
@@ -28,25 +28,26 @@ def get_column_labels(df):
             labels_dict[name] = label
     return labels_dict
 
-
+# Map values if int
 def map_value_labels(survey_data, questionnaire):
     choiceList = pd.json_normalize(questionnaire['choiceList']).dropna()
     choices = choiceList.explode('choices')
 
-    categories_dict = {}
+    categories_dict = dict()
     for _, row in choices.iterrows():
         name = row["name"]
         choice = row["choices"]
         if name in categories_dict:
-            categories_dict[name].update({choice["name"]: choice["label"]})
+            categories_dict[name].update({int(choice["name"]): choice["label"]})
         else:
-            categories_dict[name] = {choice["name"]: choice["label"]}
+            categories_dict[name] = {int(choice["name"]): choice["label"]}
 
     # Map the categories to survey_data
     survey_data_value_labels = survey_data.copy()
     for col in survey_data_value_labels.columns:
         if col in categories_dict:
             category_dict = categories_dict[col]
-            survey_data_value_labels[col] = survey_data_value_labels[col].apply(lambda x: category_dict.get(str(x), x))
+            survey_data_value_labels[col] = survey_data_value_labels[col].apply(lambda x: category_dict.get(x, x))
 
     return survey_data_value_labels
+
