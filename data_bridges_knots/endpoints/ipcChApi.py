500b940e5f781be916160400e7d777cca15ea71a
AlexGherardelli <alessandra.gherardelli@gmail.com>
2026-09-08 16:21:46 +0200

    WIP get_ipc_historical_data fix

--- data_bridges_knots/endpoints/ipcChApi.py
+++ data_bridges_knots/endpoints/ipcChApi.py
@@ -62,67 +62,118 @@ class IpcchApi:
             )
             raise e
 
-    def get_ipc_and_equivalent_latest_peaks(self, iso3: str | None = None, page: int | None = 1) -> pd.DataFrame:
-        with data_bridges_client.ApiClient(self.configuration) as api_client:
-        # Create an instance of the API class
-        api_instance = data_bridges_client.IpcchApi(api_client)
-        env = self.env # str | Environment.   * `prod` - api.vam.wfp.org   * `dev` - dev.api.vam.wfp.org (optional)
-
-        try:
-            # Retrieves a paginated list of the latest IPCCH and Equivalent peaks data, optionally filtered by ISO3 country code.
-            api_response = api_instance.ipcch_ipcch_and_equivalent_latest_peaks_get(iso3=iso3, page=page, env=env)
-            logger.info("The response of IpcchApi->ipcch_ipcch_and_equivalent_latest_peaks_get:\n")
-            df = pd.DataFrame([item.to_dict() for item in api_response.items])
-            return df
-        except Exception as e:
-            logger.error("Exception when calling IpcchApi->ipcch_ipcch_and_equivalent_latest_peaks_get: %s\n" % e)
-            raise e
-
-    def get_ipc_and_equivalent_most_recent(self, iso3: str | None = None, page: int | None = 1) -> pd.DataFrame:
+    def get_ipc_and_equivalent_latest_peaks(
+        self, iso3: str | None = None, page: int | None = 1
+    ) -> pd.DataFrame:
         with data_bridges_client.ApiClient(self.configuration) as api_client:
             # Create an instance of the API class
             api_instance = data_bridges_client.IpcchApi(api_client)
-            env = self.env # str | Environment.   * `prod` - api.vam.wfp.org   * `dev` - dev.api.vam.wfp.org (optional)
+            env = (
+                self.env
+            )  # str | Environment.   * `prod` - api.vam.wfp.org   * `dev` - dev.api.vam.wfp.org (optional)
 
             try:
-                # Retrieves a paginated list of the most recent IPCCH and Equivalent data, optionally filtered by ISO3 country code.
-                api_response = api_instance.ipcch_ipcch_and_equivalent_most_recent_get(iso3=iso3, page=page, env=env)
-                logger.info("The response of IpcchApi->ipcch_ipcch_and_equivalent_most_recent_get:\n")
+                # Retrieves a paginated list of the latest IPCCH and Equivalent peaks data, optionally filtered by ISO3 country code.
+                api_response = api_instance.ipcch_ipcch_and_equivalent_latest_peaks_get(
+                    iso3=iso3, page=page, env=env
+                )
+                logger.info(
+                    "The response of IpcchApi->ipcch_ipcch_and_equivalent_latest_peaks_get:\n"
+                )
                 df = pd.DataFrame([item.to_dict() for item in api_response.items])
                 return df
             except Exception as e:
-                logger.error("Exception when calling IpcchApi->ipcch_ipcch_and_equivalent_most_recent_get: %s\n" % e)
+                logger.error(
+                    "Exception when calling IpcchApi->ipcch_ipcch_and_equivalent_latest_peaks_get: %s\n"
+                    % e
+                )
                 raise e
 
-    def get_ipc_and_equivalent_peaks_wfp_dashboard(self, iso3: str | None = None, page: int | None = 1) -> pd.DataFrame:
+    def get_ipc_and_equivalent_most_recent(
+        self, iso3: str | None = None, page: int | None = 1
+    ) -> pd.DataFrame:
+        with data_bridges_client.ApiClient(self.configuration) as api_client:
+            # Create an instance of the API class
+            api_instance = data_bridges_client.IpcchApi(api_client)
+            env = (
+                self.env
+            )  # str | Environment.   * `prod` - api.vam.wfp.org   * `dev` - dev.api.vam.wfp.org (optional)
+
+            try:
+                # Retrieves a paginated list of the most recent IPCCH and Equivalent data, optionally filtered by ISO3 country code.
+                api_response = api_instance.ipcch_ipcch_and_equivalent_most_recent_get(
+                    iso3=iso3, page=page, env=env
+                )
+                logger.info(
+                    "The response of IpcchApi->ipcch_ipcch_and_equivalent_most_recent_get:\n"
+                )
+                df = pd.DataFrame([item.to_dict() for item in api_response.items])
+                return df
+            except Exception as e:
+                logger.error(
+                    "Exception when calling IpcchApi->ipcch_ipcch_and_equivalent_most_recent_get: %s\n"
+                    % e
+                )
+                raise e
+
+    def get_ipc_and_equivalent_peaks_wfp_dashboard(
+        self, iso3: str | None = None, page: int | None = 1
+    ) -> pd.DataFrame:
         # Enter a context with an instance of the API client
         with data_bridges_client.ApiClient(self.configuration) as api_client:
             # Create an instance of the API class
             api_instance = data_bridges_client.IpcchApi(api_client)
-            env = self.env # str | Environment.   * `prod` - api.vam.wfp.org   * `dev` - dev.api.vam.wfp.org (optional)
+            env = (
+                self.env
+            )  # str | Environment.   * `prod` - api.vam.wfp.org   * `dev` - dev.api.vam.wfp.org (optional)
 
             try:
                 # Retrieves a paginated list of IPCCH and Equivalent Peaks data for the WFP Dashboard.
-                api_response = api_instance.ipcch_ipcch_and_equivalent_peaks_wfp_dashboard_get(iso3=iso3, page=page, env=env)
-                logger.info("The response of IpcchApi->ipcch_ipcch_and_equivalent_peaks_wfp_dashboard_get:\n")
+                api_response = (
+                    api_instance.ipcch_ipcch_and_equivalent_peaks_wfp_dashboard_get(
+                        iso3=iso3, page=page, env=env
+                    )
+                )
+                logger.info(
+                    "The response of IpcchApi->ipcch_ipcch_and_equivalent_peaks_wfp_dashboard_get:\n"
+                )
                 df = pd.DataFrame([item.to_dict() for item in api_response.items])
                 return df
             except Exception as e:
-                logger.error("Exception when calling IpcchApi->ipcch_ipcch_and_equivalent_peaks_wfp_dashboard_get: %s\n" % e)
+                logger.error(
+                    "Exception when calling IpcchApi->ipcch_ipcch_and_equivalent_peaks_wfp_dashboard_get: %s\n"
+                    % e
+                )
                 raise e
 
-    def get_ipc_historical_data(self, from_date: str, to_date: str, iso3: str | None = None, page: int | None = 1) -> pd.DataFrame:
+    def get_ipc_historical_data(
+        self,
+        from_date: str | None = None,
+        to_date: str | None = None,
+        iso3: str | None = None,
+        page: int | None = 1,
+    ) -> pd.DataFrame:
+
         with data_bridges_client.ApiClient(self.configuration) as api_client:
             # Create an instance of the API class
             api_instance = data_bridges_client.IpcchApi(api_client)
-            env = self.env # str | Environment.   * `prod` - api.vam.wfp.org   * `dev` - dev.api.vam.wfp.org (optional)
+            env = (
+                self.env
+            )  # str | Environment.   * `prod` - api.vam.wfp.org   * `dev` - dev.api.vam.wfp.org (optional)
 
             try:
                 # Retrieves a paginated list of IPCCH and Equivalent Historical Data.
-                api_response = api_instance.ipcch_ipcch_historical_data_get(from_date=from_date, to_date=to_date, iso3=iso3, page=page, env=env)
-                logger.info("The response of IpcchApi->ipcch_ipcch_historical_data_get:\n")
+                api_response = api_instance.ipcch_ipcch_historical_data_get(
+                    fromDate=from_date, toDate=to_date, iso3=iso3, page=page, env=env
+                )
+                logger.info(
+                    "The response of IpcchApi->ipcch_ipcch_historical_data_get:\n"
+                )
                 df = pd.DataFrame([item.to_dict() for item in api_response.items])
                 return df
             except Exception as e:
-                logger.error("Exception when calling IpcchApi->ipcch_ipcch_historical_data_get: %s\n" % e)
+                logger.error(
+                    "Exception when calling IpcchApi->ipcch_ipcch_historical_data_get: %s\n"
+                    % e
+                )
                 raise e
